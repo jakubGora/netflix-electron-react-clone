fdefe72a38ea0a799295327550d91e74c0bc9c06
jakubGora <jakub_gora@wp.pl>
2022-05-24 20:43:10 +0200

    Api lists and top list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TopMenu from "./components/TopMenu/TopMenu";
 import TopTriller from "./components/TopTriller/TopTriller";
 import React, { useState } from "react";
 import TopList from "./components/TopList/TopList";
+import VideoList from "./components/VideoList/VideoList";
 function App() {
   const [menuActive, setMenuActive] = useState(true);
   return (
@@ -11,7 +12,14 @@ function App() {
       <TopBar setMenuActive={setMenuActive} />
       <TopMenu menuActive={menuActive} />
       <TopTriller />
-      <TopList />
+      <TopList videoOrSeries={"Movie"} title={"Top 10 filmów"} />
+      <VideoList category={"Comedy"} title={"Najlepsze komedie"} />
+      <TopList videoOrSeries={"Series"} title={"Top 10 seriali"} />
+      <VideoList category={"Drama"} title={"Najlepsze dramaty"} />
+      <VideoList category={"Action"} title={"Filmy akcji"} />
+      <VideoList category={"Horror"} title={"Filmy grozy"} />
+      <VideoList category={"Romance"} title={"Najlepsze komedie romantyczne"} />
+      <div className="space"></div>
     </div>
   );
 }
--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import "./TopList.css";
+import "./VideoList.css";
 import ReactPlayer from "react-player";
-function TopList(props) {
+function VideoList(props) {
   const [currentVideo, setCurrentVideo] = useState();
   const [topList, setTopList] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/netflix/${props.videoOrSeries}`)
+    fetch(`http://localhost:8080/netflix/cat/${props.category}`)
       .then((response) => {
         return response.json();
       })
@@ -24,48 +24,44 @@ function TopList(props) {
   };
 
   return (
-    <div className="TopList">
+    <div className="VideoList">
       <h1>{props.title}</h1>
       <div className="bt"></div>
       <div className="list">
-        {topList.length > 0
+        {topList
           ? topList.map((e, i) => (
               <div
                 onMouseEnter={() => videoMouseEnterHandler(e)}
                 onMouseLeave={() => videoMouseLeaveHandler(e)}
                 className="elem"
               >
-                {i + 1}
-                <img src={e.image} alt="img" />
+                <img src={e.poster} alt="img" />
               </div>
             ))
           : ""}
         <div className="trailer-mini">
           {" "}
-          {props.videoOrSeries != "Series" ? (
-            <ReactPlayer
-              loop={true}
-              className="react-player"
-              width={"100%"}
-              height={"100%"}
-              config={{
-                youtube: {
-                  playerVars: {
-                    showinfo: 0,
-                    mute: 1,
-                    autoplay: 1,
-                    controls: 0,
-                    loop: 1,
-                    modestbranding: 1,
-                    disablekb: 0,
-                  },
+          <ReactPlayer
+            loop={true}
+            className="react-player"
+            width={"100%"}
+            height={"100%"}
+            config={{
+              youtube: {
+                playerVars: {
+                  showinfo: 0,
+                  mute: 1,
+                  autoplay: 1,
+                  controls: 0,
+                  loop: 1,
+                  modestbranding: 1,
+                  disablekb: 0,
                 },
-              }}
-              url={currentVideo?.tmdbTrailer}
-            />
-          ) : (
-            <img src={currentVideo?.image} alt="" />
-          )}
+              },
+            }}
+            url={currentVideo?.tmdbTrailer}
+          />
+          {/* <img src={currentVideo?.image} alt="" /> */}
           <div className="info">
             <h1>{currentVideo?.title}</h1>
             <h2>{currentVideo?.genre}</h2>
@@ -78,4 +74,4 @@ function TopList(props) {
   );
 }
 
-export default TopList;
+export default VideoList;
